e3a9dbbc7498ff7d3bf8770cb1174be86e7241b5
Eugeny Konstantinov <jskonst@yandex.ru>
2022-02-18 18:34:49 +0300

    Add bubble sort sample

--- src/sorting/bubble_sort.py
+++ src/sorting/bubble_sort.py
@@ -8,13 +8,26 @@
 2 3 1 4
 2 1 3 4
 1 2 3 4
+>>> sys.stdin = io.StringIO(chr(10).join(['4','1 2 3 4']))  # input
+>>> bubble_sort()
+0
 """
 
 def bubble_sort():
-    n = input()
+    n = int(input())
     inp_string = input()
     str_lst = inp_string.split(" ")
-    print(str_lst)
+    int_lst = list(map(int, str_lst))
+    changes_count = 0
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if int_lst[j] > int_lst[j+1]:
+                int_lst[j],int_lst[j+1] = (int_lst[j+1],int_lst[j])
+                changes_count += 1
+                print(" ".join(map(str, int_lst)))  
+    if changes_count == 0: 
+        print(0)
+    
 
 if __name__ == "__main__":
     import doctest
