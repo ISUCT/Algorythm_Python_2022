039c5e57a74da5bfde89270efe14c1ed8cfd3e67
Alexandr Konichenko <jfa35ru@gmail.com>
2022-04-17 18:33:37 +0300

    who cares

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-print('74  87 65 83  72 69 82 69 ')
+import random
+
+if __name__ == '__main__':
+    random.seed(1337)
+    print(''.join(chr(random.randrange(256) ^ c) for c in bytes.fromhex('1EE4EA0944EC87A6FA604E1B') if random.randrange(2)))
